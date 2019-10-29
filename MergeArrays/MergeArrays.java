14e5f480e16980a757bc822241beea75f7f6d0b3
dharshakch97-cloud <dharshak.ch97@msitprogram.net>
2019-10-29 16:57:56 +0530

    MergeArrays Done

---
+++ MergeArrays/MergeArrays.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+class MergeArrays {
+    public static void main(String[] args) {
+        
+        int i = 0,j = 0,k = 0;
+
+        int[] a = {2,3,7,9,10};
+        int[] b = {3,4,7,110,120};
+        int[] c = new int[a.length+b.length];
+
+        while (i < a.length && j < b.length) {
+            if (a[i] < b[j]) {
+                c[k++] = a[i++];
+            }
+            else {
+                c[k++] = b[j++];
+            }
+        }
+        while (i < a.length)
+            c[k++] = a[i++];
+        while (j < b.length)
+            c[k++] = b[j++];
+
+        /* while (i < a.length) {
+            c[k++] = a[i++];
+        }
+        while (j < b.length) {
+            c[k++] = b[j++];
+        }
+        Arrays.sort(c); */
+        System.out.println(Arrays.toString(c));
+    }
+
+}
