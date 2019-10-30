1e9f46a21dd0ee886fd7fe2e445f37a511335fa7
dharshakch97-cloud <dharshak.ch97@msitprogram.net>
2019-10-30 17:44:13 +0530

    Elementary Sorting Done

--- Module-3 Elementary Sorting/1/Solution.java
+++ Module-3 Elementary Sorting/1/Solution.java
@@ -1,7 +1,21 @@
 class Solution{
 	public int[] sortInsertion(int[] arr){
 		// fill you code Here
-		int t;
+        for (int i = 0; i < arr.length; i++) {
+            int f = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > f) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = f;
+        }
+		return arr;
+	}
+	public int[] sortSelection(int[] arr){
+		// fill you code Here
+        int t;
 		for (int i = 0; i < arr.length - 1; i++) {
             int min = i;
             for (int j = i + 1; j < arr.length; j++) {
@@ -15,20 +29,6 @@ class Solution{
             arr[i] = t;
 
         }
-		return arr;
-	}
-	public int[] sortSelection(int[] arr){
-		// fill you code Here
-		for (int i = 0; i < arr.length; i++) {
-            int f = arr[i];
-            int j = i - 1;
-
-            while (j >= 0 && arr[j] > f) {
-                arr[j + 1] = arr[j];
-                j--;
-            }
-            arr[j + 1] = f;
-        }
-		return arr;
+        return arr;
 	}
 }
