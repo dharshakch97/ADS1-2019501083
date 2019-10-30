eab89696246c01c76e1d097f4eb86203af7e6403
dharshakch97-cloud <dharshak.ch97@msitprogram.net>
2019-10-30 13:56:12 +0530

    InsertionSort Done

---
+++ Module-3 Elementary Sorting/Insertion Sort/InsertionSort.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+class InsertionSort {
+    public static void main(String[] args) {
+        
+        int[] a = {5, 4, 9, 12, 10, 20, 5, 1};
+
+        for (int i = 0; i < a.length; i++) {
+            int f = a[i];
+            int j = i - 1;
+
+            while (j >= 0 && a[j] > f) {
+                a[j + 1] = a[j];
+                j--;
+            }
+            a[j + 1] = f;
+        }
+        System.out.println(Arrays.toString(a));
+    }
+}
