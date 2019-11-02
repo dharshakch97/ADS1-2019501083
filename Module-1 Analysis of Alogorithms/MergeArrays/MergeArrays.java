002dbc46057ba7bb5d11b6591e9f2ec482f9af0d
dharshakch97-cloud <dharshak.ch97@msitprogram.net>
2019-11-02 10:19:24 +0530

    Module 1 Done

--- Module-1 Analysis of Alogorithms/MergeArrays/MergeArrays.java
+++ Module-1 Analysis of Alogorithms/MergeArrays/MergeArrays.java
@@ -1,35 +1,37 @@
-// @author Sai Dharshak
+/** 
+ * @author Dharshak 
+ * 
+ * */ 
 import java.util.Arrays;
 class MergeArrays {
+
+    public int[] merge(int[] a, int[] b) {
+        int i = 0, j = 0,  k = 0;
+        int[] c = new int[a.length+b.length];
+
+            while (i < a.length && j < b.length) {
+                if (a[i] < b[j]) {
+                    c[k++] = a[i++];
+                }
+                else {
+                    c[k++] = b[j++];
+                }
+            }
+            while (i < a.length)
+                c[k++] = a[i++];
+            while (j < b.length)
+                c[k++] = b[j++];
+        return c;
+    }
     public static void main(String[] args) {
-        
-        int i = 0,j = 0,k = 0;
 
         int[] a = {2,3,7,9,10};
         int[] b = {3,4,7};
-        int[] c = new int[a.length+b.length];
 
-        while (i < a.length && j < b.length) {
-            if (a[i] < b[j]) {
-                c[k++] = a[i++];
-            }
-            else {
-                c[k++] = b[j++];
-            }
-        }
-        while (i < a.length)
-            c[k++] = a[i++];
-        while (j < b.length)
-            c[k++] = b[j++];
-
-        System.out.println(Arrays.toString(c));
-        /* while (i < a.length) {
-            c[k++] = a[i++];
-        }
-        while (j < b.length) {
-            c[k++] = b[j++];
-        }
-        Arrays.sort(c); */ // Using Arrays.sort();
+        MergeArrays ma = new MergeArrays();
+        int[] sorted = ma.merge(a, b);
+        System.out.println(Arrays.toString(sorted));
+        
     }
 
 }
--- Module-1 Analysis of Alogorithms/ThreeSum/ThreeSum.java
+++ Module-1 Analysis of Alogorithms/ThreeSum/ThreeSum.java
@@ -2,13 +2,38 @@
 import java.util.Arrays;
 
 class ThreeSum {
-    public static void main(String[] args) {
-        int[] a = {0,0,1};
-        // int[] a = {0, -1, 2, -3, 1};
 
-        Arrays.sort(a);
-        
+    public void threesum(int[] a) {
         int count = 0;
+        for (int i = 0; i < a.length - 1; i++) {
+            int l = i + 1;
+            int h = a.length - 1;
+
+            while (l < h) {
+                if (a[i] + a[l] + a[h] == 0) {
+                    count++;
+                    l++;
+                    h--;
+                }
+                else if (a[i] + a[l] + a[h] < 0)
+                    l++;
+                else
+                    h--;
+            }
+        }
+        System.out.println(count); //O(n^2)
+    }
+    public static void main(String[] args) {
+        ThreeSum ts = new ThreeSum();
+
+        int[] a = {0,0,1};
+        Arrays.sort(a);
+        ts.threesum(a);
+
+        int[] b = {0, -1, 2, -3, 1};
+        Arrays.sort(b);
+        ts.threesum(b);
+
 
         // for (int i = 0; i < a.length - 2; i++) {
         //     for (int j = i + 1; j < a.length - 1; j++) {
@@ -20,25 +45,6 @@ class ThreeSum {
         // }
         // System.out.println(countOn3); //O(n^3)
 
-        for (int i = 0; i < a.length - 1; i++) {
-            int l = i + 1;
-            int h = a.length - 1;
-
-            while (l < h) {
-
-                if (a[i] + a[l] + a[h] == 0) {
-                    count++;
-                    l++;
-                    h--;
-                }
-                else if (a[i] + a[l] + a[h] < 0)
-                    l++;
-                else
-                    h--;
-
-            }
-        }
-        System.out.println(count); //O(n^2)
 
         // for (int i = 0; i < a.length; i++) {
         //     for (int j = i + 1; j < a.length; j++) {
