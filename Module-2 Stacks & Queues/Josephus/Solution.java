57b0d15f2b93d6a3d9445d787c2ac0ea543521db
dharshakch97-cloud <dharshak.ch97@msitprogram.net>
2019-11-02 23:22:02 +0530

    Josephus Done

--- Module-2 Stacks & Queues/Josephus/Solution.java
+++ Module-2 Stacks & Queues/Josephus/Solution.java
@@ -1,54 +1,19 @@
+import java.util.*;
 class Solution {
-	public static String Josephus(int a, int b){
-		// fill you code Here
-        CircularDoubleLL josephus = new CircularDoubleLL();
-
-        for (int i = 0; i < a; i++)
-            josephus.add(i);
-       josephus.formCircle();
-
-       int p = 1;
-       Node t = josephus.head;
-
-       int e = 0;
-       String toRet = "";
-
-       while (e != a) {
-           t = t.next;
-           p++;
-
-           if (p == b) {
-               p = 1;
-               toRet += t.data + " ";
-
-               Node prevNode = t.prev;
-               Node nextNode = t.next;
-
-               prevNode.next = nextNode;
-               nextNode.prev = prevNode;
-
-               e++;
-               t = t.next;
-            }
-       }
-       toRet = toRet.trim();
-       return toRet;
+	public static String Josephus(int a, int b) {
+		String s = "";
+		Queue<Integer> queue = new LinkedList<>();
+		for(int i = 0; i < a; i++) {
+			queue.add(i);
+		}  
+		while(queue.size()>1) {
+			for(int i = 0; i < b - 1; i++) {
+				queue.add(queue.remove());
+			}
+			s = s + queue.remove() + " ";
+		}
+		s = s + queue.remove();
+		return s;
 	}
 }
-
-        // String s = "";
-        // // Queue queue = new Queue();
-        // Queue queue = new Queue();
-        // for (int i = 0; i < a; i++)
-        //     queue.enqueue(i);
-
-        // while (!queue.isEmpty()) {
-        //     for (int i = 0; i < b-1; i++)
-        //         queue.enqueue(queue.dequeue());
-        //     // System.out.println(queue.dequeue() + " ");
-        //     s = s + queue.dequeue() + " ";
-        // } 
-        // // System.out.println();
-        // s += queue.dequeue();
-		// return s;
-
+	
