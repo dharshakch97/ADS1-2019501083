8d6a6f636eb96566d4a4bf9694d79468f1671302
dharshakch97-cloud <dharshak.ch97@msitprogram.net>
2019-11-03 17:52:25 +0530

    MergeArrays Done

--- Module-1 Analysis of Alogorithms/MergeArrays/MergeArrays.java
+++ Module-1 Analysis of Alogorithms/MergeArrays/MergeArrays.java
@@ -4,11 +4,15 @@
  * */ 
 import java.util.Arrays;
 class MergeArrays {
-
+    /**
+     * @param a integer sorted array
+     * @param b integer sorted array
+     */
     public int[] merge(int[] a, int[] b) {
         int i = 0, j = 0,  k = 0;
+         // create c integer array of length a.length + b.length
         int[] c = new int[a.length+b.length];
-
+            // Merging the two arrays a and b
             while (i < a.length && j < b.length) {
                 if (a[i] < b[j]) {
                     c[k++] = a[i++];
@@ -21,16 +25,16 @@ class MergeArrays {
                 c[k++] = a[i++];
             while (j < b.length)
                 c[k++] = b[j++];
-        return c;
+        return c; // return c integer array
     }
     public static void main(String[] args) {
 
         int[] a = {2,3,7,9,10};
         int[] b = {3,4,7};
 
-        MergeArrays ma = new MergeArrays();
-        int[] sorted = ma.merge(a, b);
-        System.out.println(Arrays.toString(sorted));
+        MergeArrays ma = new MergeArrays(); // Creating object 'ma' for MergeArrays class
+        int[] sorted = ma.merge(a, b); // calling merge(a, b) method
+        System.out.println(Arrays.toString(sorted)); // print the sorted array
         
     }
 
