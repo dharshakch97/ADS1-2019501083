515846d396ffa4dc0f3787ddb1850e221820ae1a
dharshakch97-cloud <dharshak.ch97@msitprogram.net>
2019-11-03 18:13:26 +0530

    Done

--- Module-1 Analysis of Alogorithms/ThreeSum/ThreeSum.java
+++ Module-1 Analysis of Alogorithms/ThreeSum/ThreeSum.java
@@ -2,7 +2,10 @@
 import java.util.Arrays;
 
 class ThreeSum {
-
+    /**
+     * @param a integer array
+     * Count the number of triplets that equal to sum zero.
+     */
     public void threesum(int[] a) {
         int count = 0;
         for (int i = 0; i < a.length - 1; i++) {
@@ -10,7 +13,8 @@ class ThreeSum {
             int h = a.length - 1;
 
             while (l < h) {
-                if (a[i] + a[l] + a[h] == 0) {
+                // count increments if sum of three values equals zero
+                if (a[i] + a[l] + a[h] == 0) { 
                     count++;
                     l++;
                     h--;
@@ -33,26 +37,6 @@ class ThreeSum {
         int[] b = {0, -1, 2, -3, 1};
         Arrays.sort(b);
         ts.threesum(b);
-
-
-        // for (int i = 0; i < a.length - 2; i++) {
-        //     for (int j = i + 1; j < a.length - 1; j++) {
-        //         for (int k = j + 1; k < a.length; k++) {
-        //             if (a[i] + a[j] + a[k] == 0)
-        //                 countOn3++;
-        //         }
-        //     }
-        // }
-        // System.out.println(countOn3); //O(n^3)
-
-
-        // for (int i = 0; i < a.length; i++) {
-        //     for (int j = i + 1; j < a.length; j++) {
-        //         if (Arrays.binarySearch(a, j + 1, a.length, -(a[i] + a[j])) > 0)
-        //             countOnBinary++;
-        //     }
-        // }
-        // System.out.println(countOnBinary);
     }
 }
 
