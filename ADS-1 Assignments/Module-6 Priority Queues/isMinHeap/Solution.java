8847c7755f7167eab4ca853159706c28fe9149e5
dharshakch97-cloud <dharshak.ch97@msitprogram.net>
2019-11-08 11:30:37 +0530

    isMinHeap Done

--- ADS-1 Assignments/Module-6 Priority Queues/isMinHeap/Solution.java
+++ ADS-1 Assignments/Module-6 Priority Queues/isMinHeap/Solution.java
@@ -1,13 +1,19 @@
+/**
+ * @author Dharshak
+ */
 class Solution {
-	public static boolean isMinHeap(double[] arr) {
-		if (arr.length <= 0)
-			return false;
-		for (int i = 0; i < arr.length-1; i++) {
-			if ((2*i+1) < arr.length && (2*i+ 2) < arr.length) {
+	/**
+	 * @param arr input array
+	 */
+	public static boolean isMinHeap(double[] arr) { // input array
+		if (arr.length <= 0) 
+			return false; // if array length <= 0
+		for (int i = 0; i < arr.length-1; i++) { 
+			if ((2*i+1) < arr.length && (2*i+ 2) < arr.length) { // check 2*i+1 and 2*i+2 are within the array length
 				if (arr[2*i+1]<arr[i] && arr[2*i+2] <arr[i])
-					return false;
+					return false; // return false check arr[2*i+1], arr[2*i+2] lessthan arr[i]
 			}
 		}
-		return true;
+		return true; // else, return true
 	}
 }
