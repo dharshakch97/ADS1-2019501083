e3fabaef0f36e072fc566a58d5799d3a32969d34
dharshakch97-cloud <dharshak.ch97@msitprogram.net>
2019-11-10 19:02:02 +0530

    RansomNote Done

--- ADS-1 Assignments/Module-9 HashTables/RansomNote/RansomNote.java
+++ ADS-1 Assignments/Module-9 HashTables/RansomNote/RansomNote.java
@@ -1,81 +1,58 @@
-/**
- * @author Dharshak
- */
-import java.util.Scanner;
-/**
- * class for HashTable
- */
-class HashTable {
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
-    SeparateChainingHashST<String, Integer> hashTable; // creating hashTable using seperate chaining
+public class RansomNote {
 
-    /**
-     * Constructor for HashTable()
-     */
-    HashTable() {
-        this.hashTable = new SeparateChainingHashST<String, Integer>();
+    public static boolean canMake(String[] magazine, String[] ransom){
+        
+        Map<String, Integer> magazineHashMap = new HashMap<String, Integer>();
+        Map<String, Integer> ransomHashMap = new HashMap<String, Integer>();
+
+        for(String w: magazine){
+            if (!magazineHashMap.containsKey(w)) 
+                magazineHashMap.put(w, 0);
+            magazineHashMap.put(w, magazineHashMap.get(w) + 1);
+        }
+
+        for(String w: ransom){
+            if (!ransomHashMap.containsKey(w)) 
+                ransomHashMap.put(w, 0);
+            ransomHashMap.put(w, ransomHashMap.get(w) + 1);
+        }
+
+        for(String key: ransomHashMap.keySet()){
+            if (!magazineHashMap.containsKey(key)) 
+                return false;
+            int needed = ransomHashMap.get(key);
+            if (magazineHashMap.get(key) < needed) 
+                return false;
+        }
+        return true;
     }
 
-    /**
-     * add(String word) checks if the given word is in hashTable or not 
-     * if word is found, get the frequency of that word and update the frequency
-     * or else, add word to the hashTable
-     */
-    public void add(String w) {
-        if (hashTable.contains(w)) { // if true
-            Integer old = hashTable.get(w);
-            hashTable.put(w, old + 1);
-        } else {
-            hashTable.put(w, 1);
-        }
-    }
-
-    /**
-     * CheckAndUpdate(String w) checks the word and updates it's frequency
-     * if word is found, get the frequency of that word and decrements it's frequency
-     * if word fequency equals zero, delete the word.
-     */
-    public boolean CheckAndUpdate(String w) {
-        if (hashTable.contains(w)) {
-            Integer old = hashTable.get(w);
-            hashTable.put(w, old - 1);
-            if (hashTable.get(w) == 0) {
-                hashTable.delete(w);
-            }
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-}
-
-class RansomNote {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in); // SCaanner class to take input from the user
-        HashTable hashTable = new HashTable(); // Hashtable object is created to use methods in Hashtable.
-        String[] magazine = scan.nextLine().split(" "); // split the input by space
-        for (String eachWord : magazine) {
-            hashTable.add(eachWord); // add each word to magazine
-
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt();
+        int n = scan.nextInt();
+        String mgzn[] = new String[m]; // magazine string array
+        for (int i = 0; i < m; i++) {
+            mgzn[i] = scan.next(); //read magazine string
         }
-
-        String[] noteWords = scan.nextLine().split(" "); // take another input from user
-        boolean can = true;
-        for (String eachWord : noteWords) { // iterate each word
-            if (hashTable.CheckAndUpdate(eachWord)) {// check each word, if it is present in the hashtable
-                                                     // if present decrement the frequency by 1.
-            } else { // if word not present
-                can = false; // set canMake to false
-                break;
-            }
-
+        String ransom[] = new String[n]; //ransom String array
+        for(int i = 0; i < n; i++){
+            ransom[i] = scan.next(); // read ransom string
         }
-        if (can) {// if true
-            System.out.println("YES"); // print "YES"
-        } else {// else
-            System.out.println("NO");// print "NO"
-        }
-
+        /*call canMake(mgzn, ransom) method
+        if the canMake method returns True, print("YES*)
+        otherwise, return False
+        */
+        if (canMake(mgzn, ransom))
+            System.out.println("YES"); // print YES
+        else
+            System.out.println("NO"); // print NO
     }
+
 }
