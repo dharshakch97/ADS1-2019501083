7c4d46a5984b8762e8a3b98abe4c93b6aa6e0b7e
dharshakch97-cloud <dharshak.ch97@msitprogram.net>
2019-11-18 10:59:07 +0530

    DeckOfCards Done

--- ADS-1 Practice/Assignment Practice/DeckOfCards/DeckOfCards.java
+++ ADS-1 Practice/Assignment Practice/DeckOfCards/DeckOfCards.java
@@ -1,17 +1,27 @@
+/**
+ * @author Dharshak
+ */
 import java.util.*;
+/**
+ * Card class implementation
+ * Card implements Comparable<Card> 
+ */
 class Card implements Comparable<Card> {
-    String suit_value;
-    int value;
+    String suit_value; // suit_value of 'String' type
+    int value; // value of 'int' type
 
+    // Card constructor
     Card(String s, int v) {
         suit_value = s;
         value = v;
     }
 
+    // toString method for returning suit_value and value to String
     public String toString() {
         return suit_value + " " + value;
     }
 
+    // compareTo method for comparing Card objects
     public int compareTo(Card t) {
         if (this.suit_value.compareTo(t.suit_value) < 0)
             return 1;
@@ -27,6 +37,10 @@ class Card implements Comparable<Card> {
 
 class DeckOfCards {
     
+    /**
+     * Shuffle the cards in the deck array
+     * @param deck Card[] type array
+     */
     public Card[] shuffle_cards(Card[] deck) {
 
         for (int i = 0; i < deck.length; i++) {
@@ -36,9 +50,13 @@ class DeckOfCards {
             deck[j] = deck[i];
             deck[i] = t;
         }
-        return deck;
+        return deck; // return the shuffled deck array
     }
 
+    /**
+     * Sort the cars in the deck array
+     * @param deck Card[] type array
+     */
     public Card[] cards_sort(Card[] deck) {
         for (int i = 0; i < deck.length; i++) {
             for (int j = i ; j > 0 && deck[j].compareTo(deck[j - 1]) < 0; j--) {
@@ -47,26 +65,27 @@ class DeckOfCards {
                 deck[j - 1] = t;
             }
         }
-        return deck;
+        return deck; // return the sorted deck array
     }
+
     public static void main(String[] args) {
 
-        String[] suits = {"spade","heart","clubs","diamond"};
-        int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
-        Card[] deck = new Card[52];
+        String[] suits = {"spade","heart","clubs","diamond"}; // suits array of String[] array type
+        int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}; // values array of int[] array type
+        Card[] deck = new Card[52]; // deck array of Card[] type, size of 52
 
-        DeckOfCards dc = new DeckOfCards();
+        DeckOfCards dc = new DeckOfCards(); // object creation for DeckOfCards()
         
         int index = 0;
         for (int i = 0; i < suits.length; i++) {
             for (int j = 0; j < values.length; j++) {
-                deck[index] = new Card(suits[i], values[j]);
+                deck[index] = new Card(suits[i], values[j]); // Adding 52 cards to deck array
                 index++;
             }
         }
 
-        Card[] shuffled_deck = dc.shuffle_cards(deck);
-        Card[] sorted_deck = dc.cards_sort(deck);
-        System.out.println(Arrays.toString(sorted_deck));
+        Card[] shuffled_deck = dc.shuffle_cards(deck); // shuffling the cards
+        Card[] sorted_deck = dc.cards_sort(deck); // sorting the cards
+        System.out.println(Arrays.toString(sorted_deck)); // printing the sorted array of cards deck
     }
 }
